5ed8e17b3bac07d88f02158e73cb2e1ef47aa3dd
Tomislav Pesa <topesa@vsite.hr>
2020-12-29 11:52:52 +0100

    Implement OpenButton_Click and SaveButton_Click

--- TextSortGUI/Form1.cs
+++ TextSortGUI/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace TextSortGUI
 {
     public partial class Form1 : Form
     {
+        private string text = string.Empty;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +22,38 @@ namespace TextSortGUI
 
         private void OpenButton_Click(object sender, EventArgs e)
         {
+            OpenFileDialog open = new OpenFileDialog
+            {
+                Title = "Open File",
+                Filter = "Text Files (*txt)|*txt"  // filtering .txt files because we are working only with text files
+            };
 
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(File.OpenRead(open.FileName));
+
+                text = sr.ReadToEnd(); // need separated variable for AccordingToTheSpelling 
+                TextBox.Text = text;
+
+                sr.Dispose();
+            }
         }
 
         private void SaveButton_Click(object sender, EventArgs e)
         {
+            SaveFileDialog save = new SaveFileDialog
+            {
+                Title = "Save File",
+                Filter = "Text Files (*txt)|*txt"  // filtering .txt files because we are working only with text files
+            };
 
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(File.Create(save.FileName));
+
+                sw.Write(TextBox.Text);
+                sw.Dispose();
+            }
         }
 
         private void uppercaseToolStripMenuItem_Click(object sender, EventArgs e)
